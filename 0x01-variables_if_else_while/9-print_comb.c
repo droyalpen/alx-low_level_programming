f26526d05b118f16205bb615d8080979c473306e
droyalpen <droyalpen@gmail.com>
2022-09-10 01:23:47 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9 with commas and spaces.
+ *
+ * Return: Always 0 (Success)
+ */
+	@@ -14,8 +14,8 @@ int main(void)
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
