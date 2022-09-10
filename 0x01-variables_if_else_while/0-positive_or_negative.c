0d3e46940502ae407a5af3bc90a7bb53a64f63e3
droyalpen <droyalpen@gmail.com>
2022-09-09 17:29:34 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *  * main - Entry point
+ *  * main - entry point
  *   *
- *    * Return: Always 0
- *     */
+ *    * return: always 0
+ *    
+ *    */
 int main(void)
 {
 		int n;
