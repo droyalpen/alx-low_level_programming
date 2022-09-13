aeb9e7fa893cafcc6c465a4a1d3b47afafbf6433
droyalpen <droyalpen@gmail.com>
2022-09-13 15:57:22 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,10 +7,10 @@
      */
 int _abs(int c)
 {
-		if (c > 0 || c == 0)
-				{
-						return (c);
-							}
-			else
-					return (c * -1);
+if (c > 0 || c == 0)
+{
+return (c);
+}
+	else
+return (c * -1);
 }
