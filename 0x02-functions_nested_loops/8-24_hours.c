a92e0f384afc2e4627e4363ed910a3c540022b5e
droyalpen <droyalpen@gmail.com>
2022-09-13 15:44:53 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,7 @@
  *   * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
  *    * and resets mins
  *     * Return: 0
- *      */
+ */
 void jack_bauer(void)
 {
 		int hours = 0;
