38edba7bba02aafd2e9265e43bc1c4e832957f92
droyalpen <droyalpen@gmail.com>
2022-09-12 21:49:07 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,16 @@
  *
  * Return: Always 0 (Success)
  * */
-void print_alphabet (void)
-{
-	int n;
 
-	for (n = 'a'; n <= 'z'; n++)
+int main (void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
 	{
-		_putchar(n);
+		putchar(alp[i]);
 	}
-	_putchar('/n');
+	putchar('\n');
+	return (0);
 }
