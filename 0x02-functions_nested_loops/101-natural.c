a4a6990bb5cb7b32ebd40bb859f659f15892889d
droyalpen <droyalpen@gmail.com>
2022-09-13 20:26:56 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,46 +1,20 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_times_table - Prints the n times table
+ * main - Prints natural numbers below 1024 that are
+ * multiplies of 3 or 5
  *
- * @n: number times table (0 < n <= 15)
- *
- * Return: no return
+ * Return: Always 0.
  */
-void print_times_table(int n)
+int main(void)
 {
-	int a, b, op;
+	int a, b;
 
-	if (n >= 0 && n <= 15)
+	for (a = 1; a < 1024; a++)
 	{
-		for (a = 0; a <= n; a++)
-		{
-			_putchar(48);
-			for (b = 1; b <= n; b++)
-			{
-				op = a * b;
-				_putchar(44);
-				_putchar(32);
-				if (op <= 9)
-				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(op + 48);
-				}
-				else if (op <= 99)
-				{
-					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-				else
-				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
-				}
-			}
-			_putchar('\n');
-		}
+		if ((a % 3) == 0 || (a % 5) == 0)
+			b += a;
 	}
+	printf("%d\n", b);
+	return (0);
 }
