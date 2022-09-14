b584529f46cb9de6ae27e624f4d61508d365d760
droyalpen <droyalpen@gmail.com>
2022-09-13 17:14:24 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,13 +8,11 @@
 void print_alphabet(void)
 {
 		char c = 'a';
-
 			while (c <= 'z')
-					{
-								_putchar(c);
-										c++;
-
-											}
-				_putchar('\n');
+	{
+		_putchar(c);
+		c++
+	}
+			_putchar('\n');
 
 }
