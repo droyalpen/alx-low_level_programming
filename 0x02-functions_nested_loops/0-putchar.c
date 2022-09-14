37d480b1e86d88ee71f5e99edfe30e5508ba8dd1
droyalpen <droyalpen@gmail.com>
2022-09-13 17:39:34 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * mqin - entry point
- *
- * Return: nothing, return void
- * */
-int main (void)
+ * main - print putchar
+ * Return: always 0
+ */
+
+int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+
+	_putchar ('_');
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
 
 	return (0);
 }
