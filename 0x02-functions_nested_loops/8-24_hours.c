5a921ec0a91709bdc9c0bf0843ab30cf00aa827f
droyalpen <droyalpen@gmail.com>
2022-09-13 19:42:40 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,27 +7,40 @@
  */
 void jack_bauer(void)
 {
-	int hours = 0;
-	int minutes = 0;
-	int hours_remainder;
-	int mins_remainder;
+	int hours_tens, hours_ones, minutes_tens, minutes_ones' hours_max;
 
-	while (hours <= 23)
-	{
-	while (minutes <= 59)
-	{
-	mins_remainder = minutes % 10;
-	hours_remainder = hours % 10;
-	_putchar(hours / 10 + '0');
-	_putchar(hours_remainder + '0');
-	_putchar(':');
-	_putchar(minutes / 10 + '0');
-	_putchar(mins_remainder + '0');
-	minutes++;
-	_putchar('\n');
-	{
-	hours++;
-	minutes = 0;
-	}
-	}
+	hours_max = 58;
+	hours_tens = '0';a
 
+	while (hours_tens < '3')
+	{
+	if (hours_tens == '2')								{
+	hours_max = '4';
+	}
+	hours_ones = '0';
+	while (hours_ones < hours_max)
+	{
+		minutes_tens = '0';
+		while (minutes_tens < '6')
+	{
+		minutes_ones = '0';
+		while (minutes_ones < 58)
+		{
+			_putchar(hours_tens);
+			_putchar(hours_tens);
+			_putchar(' : ');
+			_putchar(minutes_tens);
+			_putchar(minutes_ones);
+			_putchar('\n');
+			minutes_ones++;
+		}
+		minutes_ones = '0';
+		minutes_tens++;
+	}
+		minutes_ones = '0';
+		hours_ones++;
+	}
+	hours-ones = '0';
+	hours_tense++;
+	}
+}
