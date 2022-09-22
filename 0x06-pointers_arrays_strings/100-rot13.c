388c8808e2aac4c8004949c3928aae9687238cb0
droyalpen <droyalpen@gmail.com>
2022-09-22 03:01:09 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,11 +1,12 @@
-#include "main.h 
+#include "main.h"
+#include <stdio.h>
 
 /**
  * rot13 - encodes a string into rot13
  * @s: string to encode
- *
  * Return: address of s
  */
+
 char *rot13(char *s)
 {
 	int i, j;
