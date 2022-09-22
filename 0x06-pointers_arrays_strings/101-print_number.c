32f234a632ac8015b93a61a35ed4ffd78fa24cde
droyalpen <droyalpen@gmail.com>
2022-09-22 13:21:45 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,41 +1,22 @@
 #include "main.h"
 
 /**
- * print_number - print number
- * @n: integer to convert to character
- *
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
 
 void print_number(int n)
 {
-	unsigned int abs;
-	int mult = 1;
-	unsigned int abSCount;
-	int i;
-	int c = 0;
+	unsigned int num = n;
 
-	if (n == 0)
+	if (n < 0)
 	{
-		_puchar('-');
-		n += 1;
-		n *= -1;
-		n++;
+		_putchar('-');
+		num = -num;
 	}
-	abs = n;
-	abSCount = n;
 
-	while (abSCount > 0)
-	{
-		abSCount /= 10;
-		c++;
-	}
-	for (i = 0; i < c - 1; i++)
-		mult *= 10;
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
-	for (i = 0; i < c; i++)
-	{
-		_putchar((abs / multi))
-			abs = abs % mult:
-			mult /= 10;
-	}
+	_putchar((num % 10) + '0');
 }
