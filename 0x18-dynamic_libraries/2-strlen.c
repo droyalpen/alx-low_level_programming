04dc4e0d7201d0f1e1ce250a1d53cc17c106946b
droyalpen <droyalpen@gmail.com>
2022-12-16 04:55:00 -0800

    0x18

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function declared
+ * @s: initialized integer
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
