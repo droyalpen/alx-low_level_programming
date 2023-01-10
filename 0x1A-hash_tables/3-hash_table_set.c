7af60b934249538ccaf3b9ec21ba18a1ca5b6559
droyalpen <93287030+droyalpen@users.noreply.github.com>
2023-01-10 17:52:01 +0100

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,52 +1,51 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_set - creates a new node in the hashtable
- * @ht: pointer to the hashtable
+ * hash_table_set - adds an element to the hash table
+ *
+ * @ht: hash table
  * @key: key
- * @value: value to place in the node
- * Return: 1  in success 0 otherwise
+ * @value: value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node = malloc(sizeof(hash_node_t));
-	unsigned long int haidx;
-	hash_node_t *temp;
+	hash_node_t *new = NULL;
+	hash_node_t *temp = NULL;
+	int index;
 
-	if (ht == NULL || key == '\0' || *key == '\0')
+	if (ht == NULL || key == NULL || strlen(key) == 0)
 		return (0);
-	if (new_node == NULL)
-		return (0);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	haidx = key_index((unsigned char *)key, ht->size);
-	if (ht->array[haidx] != NULL)
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+/*update value for existing key*/
+	temp = ht->array[index];
+	while (temp != NULL)
 	{
-		temp = ht->array[haidx];
-		while (temp != NULL)
-		{
-			if (strcmp(temp->key, new_node->key) == 0)
-				break;
-			temp = temp->next;
-		}
-		if (temp == NULL)
-		{
-			new_node->next = ht->array[haidx];
-			ht->array[haidx] = new_node;
-		}
-		else
+		if (strcmp(temp->key, key) == 0)
 		{
 			free(temp->value);
-			temp->value = strdup(new_node->value);
-			free(new_node->value);
-			free(new_node->key);
-			free(new_node);
+			temp->value = strdup(value);
+			return (1);
 		}
+		temp = temp->next;
 	}
-	else
-	{
-		new_node->next = NULL;
-		ht->array[haidx] = new_node;
-	}
+
+/*collision*/
+	new = malloc(sizeof(hash_node_t *));
+	if (new == NULL)
+		return (0);
+
+	new->value = strdup(value);
+	new->key = strdup(key);
+	new->next = NULL;
+
+	if (ht->array[index] != NULL)
+		new->next = ht->array[index];
+
+/*no collision*/
+	ht->array[index] = new;
 	return (1);
 }
-Footer
